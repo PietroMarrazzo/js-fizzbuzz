149489dfce11f949f4703b245c14bd2f6b369316
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-20 13:34:58 +0200

    job done

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ var database = [];
 
 // riempio il database di numeri, da 1 a 100
 for (var i=1; i <= 100; i++) {
-    var number = database[i];
+    
 
     // se il numero è divisibile per 3 e 5 allora aggiungo fizzbuzz
-    if (number % 3 === 0 && number % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
         database.push('fizzbuzz');
     } 
     // altrimenti controlo se è divisibile solo pr 3
-    else if (number % 3 === 0) {
+    else if (i % 3 === 0) {
         database.push('fizz')
     }
     // altrimenti controllo se è divisibile per 5
-    else if (number % 5 === 0) {
+    else if (i % 5 === 0) {
         database.push('buzz')
     } 
     // se non è divisibile per 3 o 5, allora aggiungo il valore i 
